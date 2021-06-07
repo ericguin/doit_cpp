d08c665e913799b4e328e7b834d384cd6bd6dcb5
Eric Guinn <eric.guinn@outlook.com>
2021-06-06 22:42:53 -0600

    Mostly working with basic stuff

--- dodo.py
+++ dodo.py
@@ -7,57 +7,118 @@ class BuildContext:
     Build_Contexts = {}
     _mm_regex = re.compile(r'([\w\/]+\.h)')
 
-    def __init__(self, name, sources=[], defines=[], path=[], libs=[], libpath=[],
-                 cc=None, cxx=None, ar=None, objdir=None, replicate_structure=False, env=None):
+    def __init__(self, name, cmd_args=[], cpath=[], libs=[], libpath=[], sources=[],
+                 cc=None, cxx=None, ar=None, objdir=None, replicate_structure=False, base_dir=None, env=None):
         self.name = name
-        self.sources = sources
-        self.defines = defines
-        self.path = path
+        self.cmd_args = cmd_args
+        self.cpath = cpath
         self.libs = libs
         self.libpath = libpath
+        self.sources = sources
         self.cc = cc or "gcc"
         self.cxx = cxx or "g++"
         self.ar = ar or "ar"
-        self.objdir = objdir
+        self.objdir = Path(str(objdir)) if objdir is not None else None
         self.replicate_structure = replicate_structure
+        self.base_dir = base_dir or Path(".")
         self.env = env or os.environ.copy()
 
         BuildContext.Build_Contexts[self.name] = self
     
-    def determine_deps(self, file):
-        compiler = {"cpp": self.cxx, "c": self.cc}[Path(file).suffix]
-        mm = subprocess.check_output([compiler, '-MM', file, '-Iother_includes'], env=env).decode('utf-8')
-        ret = BuildContext._mm_regex.findall(mm)
-        print("getting deps for " + file)
-        return { 'file_dep': ret }
-
-    def add_sources(self, *sources):
-        for source in sources:
-            if isinstance(source, BuildObject):
-                self.sources += source
+    def get_file(self, file):
+        if not isinstance(file, BuildObject):
+            file = BuildObject(str(file))
+        
+        return file
+    
+    def get_compiler(self, file):
+        file = self.get_file(file)
+        compiler = {".cpp": self.cxx, ".c": self.cc,
+                    ".cc": self.cxx}[file.file.suffix]
+        return compiler
+    
+    def get_output_dir(self, file):
+        file = self.get_file(file)
+        if self.objdir is not None:
+            if self.replicate_structure:
+                return self.objdir / file.file.parent.relative_to(self.base_dir)
             else:
-                self.sources += BuildObject(str(source))
-
-class BuildObject:
-    def __init__(self, *args, **kwargs):
-        pass
-
+                return self.objdir
+        else:
+            return file.file.parent
+    
+    def get_output_file(self, file):
+        file = self.get_file(file)
+        dir = self.get_output_dir(file)
+        file = dir / file.file.with_suffix('.o') 
+        return file
+        
+    def determine_deps(self, file):
+        file = self.get_file(file)
+        compiler = self.get_compiler(file)
+        c_paths = list(set(self.cpath + file.cpath))
+        mm = subprocess.check_output([compiler, '-MM', str(file)] + 
+                                     ["-I" + s for s in c_paths], env=self.env).decode('utf-8')
+        ret = BuildContext._mm_regex.findall(mm)
+        return { 'file_dep': ret }
+    
+    def get_action(self, file, additional_args=[]):
+        file = self.get_file(file)
+        compiler = self.get_compiler(file)
+        out_path = self.get_output_file(file)
+        out_path.parent.mkdir(parents = True, exist_ok = True)
+        c_paths = list(set(self.cpath + file.cpath))
+        c_args = list(set(self.cmd_args + file.cmd_args + additional_args))
+        action = f"{compiler} -c -o  {out_path} {' '.join(c_args)} {' '.join(['-I' + s for s in c_paths])} {file}"
+        print(action)
+        return action
+    
 def task_determine_dependencies():
     """Determine dependencies for C/C++ build"""
 
-    for ctxt in BuildContext.Build_Contexts:
+    for ctxt in BuildContext.Build_Contexts.values():
         for file in ctxt.sources:
             yield {
                 'name': f"{file}_{ctxt.name}",
-                'actions': [(determine_deps, [file])],
-                'file_dep': [file]
+                'actions': [(ctxt.determine_deps, [file])],
+                'file_dep': [str(file)]
             }
+
+class BuildObject:
+    def __str__(self):
+        return self.file.as_posix()
+
+    def __init__(self, file, cpath=[], cmd_args=[]):
+        self.file = Path(file)
+        self.cmd_args = cmd_args
+        self.cpath = cpath
 
 main_sources = Path('src').glob("*.cpp")
 
-test = BuildObject()
+test = BuildObject('main.cpp', cmd_args=['-DOTHER_FILE'])
 
-main_build = BuildContext("main")
-main_build.add_sources(main_sources)
+main_build = BuildContext("main", cmd_args=['-DMAIN'], objdir='obj/build/main')
+main_build.sources += [test]
 
-print(main_build.sources)
+def task_compile():
+    """I'm gonna try and compile stuff hot stuff"""
+    
+    yield {
+        'basename': 'compile_x',
+        'name': None,
+        'doc': 'builds file x'
+    }
+
+    for ctxt in BuildContext.Build_Contexts.values():
+        for file in ctxt.sources:
+            yield {
+                'name'    : str(file),
+                'actions' : [ctxt.get_action(file)],
+                'file_dep': [str(file)],
+                'calc_dep': [f"determine_dependencies:{file}_{ctxt.name}"],
+                'targets' : [ctxt.get_output_file(file)]
+            }
+
+if __name__ == '__main__':
+    import doit
+    doit.run(globals())
