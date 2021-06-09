399e590be973b693737a3d9906b169f85a357692
Eric Guinn <eric.guinn@outlook.com>
2021-06-08 21:55:20 -0600

    Removing debug prints

--- dodo.py
+++ dodo.py
@@ -5,12 +5,10 @@ srcs = [BuildObject(f, cmd_args=['-DOTHER_FILE', '-DYES', '-DMAIN']) for f in (l
 
 main_build = BuildContext("main", output="bin/main.exe", cmd_args=['-DMAIN'], objdir='obj/build/main', cpath=['src'])
 main_build.sources.extend(srcs)
-print(main_build.sources)
 
 ut_build = BuildContext("ut", output="bin/ut.exe", cmd_args=['-DUT'], objdir='obj/build/ut', cpath=['src'])
 ut_build.sources.extend(srcs)
 ut_build.sources.extend([BuildObject("ut/ut.cpp")])
-print(main_build.sources)
 
 if __name__ == '__main__':
     import doit
