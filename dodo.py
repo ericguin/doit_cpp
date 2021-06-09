0052366db954481f939ccd42d4294afb553310f2
Eric Guinn <eric.guinn@outlook.com>
2021-06-08 20:25:03 -0600

    Linking now works!!

--- dodo.py
+++ dodo.py
@@ -1,20 +1,29 @@
 from pathlib import Path
 import subprocess
 import re
+import platform
 import os
 
 class BuildContext:
     Build_Contexts = {}
     _mm_regex = re.compile(r'([\w\/]+\.h)')
 
-    def __init__(self, name, cmd_args=[], cpath=[], libs=[], libpath=[], sources=[],
+    def _get_output_suffix():
+        if platform.system() == "Windows":
+            return ".exe"
+        else:
+            return ""
+
+    def __init__(self, name, output=None, cmd_args=None, cpath=None, libs=None, libpath=None, sources=None, link_args=None,
                  cc=None, cxx=None, ar=None, objdir=None, replicate_structure=False, base_dir=None, env=None):
         self.name = name
-        self.cmd_args = cmd_args
-        self.cpath = cpath
-        self.libs = libs
-        self.libpath = libpath
-        self.sources = sources
+        self.output = output or name + BuildContext._get_output_suffix()
+        self.cmd_args = cmd_args or list()
+        self.link_args = link_args or list()
+        self.cpath = cpath or list()
+        self.libs = libs or list()
+        self.libpath = libpath or list()
+        self.sources = sources or list()
         self.cc = cc or "gcc"
         self.cxx = cxx or "g++"
         self.ar = ar or "ar"
@@ -37,6 +46,12 @@ class BuildContext:
         compiler = {".cpp": self.cxx, ".c": self.cc,
                     ".cc": self.cxx}[suffix]
         return compiler
+
+    def get_linker(self):
+        if any(f.file.suffix == ".cpp" for f in self.sources):
+            return self.cxx
+        else:
+            return self.cc
     
     def get_output_dir(self, file):
         file = self.get_file(file)
@@ -70,14 +85,34 @@ class BuildContext:
         out_path.parent.mkdir(parents = True, exist_ok = True)
         c_paths = list(set(self.cpath + file.cpath))
         c_args = list(set(self.cmd_args + file.cmd_args + additional_args))
-        action = f"{compiler} -c -o  {out_path} {' '.join(c_args)} {' '.join(['-I' + s for s in c_paths])} {file}"
+        action = f"{compiler} -c -o  {out_path} {' '.join(c_args).strip()} {' '.join(['-I' + s for s in c_paths]).strip()} {file}"
         print(action)
         return action
     
+    def get_objects(self):
+        for file in self.sources:
+            file = self.get_file(file)
+            yield self.get_output_file(file)
+                
     def compile_file(self, file):
         file = self.get_file(file)
+        
         action = self.get_action(file)
-        return subprocess.run(action, env=self.env, capture_output=True).returncode == 0
+        return subprocess.run(action, env=self.env).returncode == 0
+    
+    def get_link_action(self):
+        objects = ' '.join([str(o) for o in list(self.get_objects())]).strip()
+        libpath = ' '.join(['-L' + l for l in self.libpath]).strip()
+        libs = f"-Wl,--start-group {' '.join(['-l' + lib for lib in self.libs]).strip()} -Wl,--end-group" if any(self.libs) else ""
+        link_args = ' '.join([arg for arg in self.link_args]).strip()
+        linker = self.get_linker()
+        action = f"{linker} -o {self.output} {objects} {libpath} {libs} {link_args}"
+        print(action)
+        return action
+
+    def link(self):
+        action = self.get_link_action()
+        return subprocess.run(action, env=self.env).returncode == 0
     
 def task_determine_dependencies():
     """Determine dependencies for C/C++ build"""
@@ -85,7 +120,7 @@ def task_determine_dependencies():
     for ctxt in BuildContext.Build_Contexts.values():
         for file in ctxt.sources:
             yield {
-                'name': f"{file}_{ctxt.name}",
+                'name': f"{file}@{ctxt.name}",
                 'actions': [(ctxt.determine_deps, [file])],
                 'file_dep': [str(file)]
             }
@@ -99,18 +134,22 @@ class BuildObject:
         self.cmd_args = cmd_args
         self.cpath = cpath
 
-main_sources = Path('src').glob("*.cpp")
-
 srcs = [BuildObject(f, cmd_args=['-DOTHER_FILE', '-DYES', '-DMAIN']) for f in (list(Path('.').glob("*.cpp")) + list(Path('.').glob('src/*.cpp')))]
 
-main_build = BuildContext("main", cmd_args=['-DMAIN'], objdir='obj/build/main', replicate_structure=True)
-main_build.sources += srcs
+main_build = BuildContext("main", cmd_args=['-DMAIN'], objdir='obj/build/main')
+main_build.sources.extend(srcs)
+print(main_build.sources)
+
+ut_build = BuildContext("ut", cmd_args=['-DUT'], objdir='obj/build/ut')
+ut_build.sources.extend(srcs)
+ut_build.sources.extend([BuildObject("ut/ut.cpp")])
+print(main_build.sources)
 
 def task_compile():
     """I'm gonna try and compile stuff hot stuff"""
     
     yield {
-        'basename': 'compile_x',
+        'basename': 'compile:x',
         'name': None,
         'doc': 'builds file x'
     }
@@ -118,12 +157,34 @@ def task_compile():
     for ctxt in BuildContext.Build_Contexts.values():
         for file in ctxt.sources:
             yield {
-                'name'    : str(file),
+                'name'    : f"{file}@{ctxt.name}",
                 'actions' : [(ctxt.compile_file, [file])],
                 'file_dep': [str(file)],
-                'calc_dep': [f"determine_dependencies:{file}_{ctxt.name}"],
-                'targets' : [ctxt.get_output_file(file)]
+                'calc_dep': [f"determine_dependencies:{file}@{ctxt.name}"],
+                'clean': True,
+                'targets' : [ctxt.get_output_file(file)],
+                'verbosity' : 2
             }
+
+def task_link():
+    """Links stuff yo"""
+
+    yield {
+        'basename': 'link:x',
+        'name': None,
+        'doc': 'links the context x'
+    }
+    
+    for ctxt in BuildContext.Build_Contexts.values():
+        yield {
+            'name': f"{ctxt.name}",
+            'actions': [ctxt.link],
+            'task_dep': [f'compile:*@{ctxt.name}'],
+            'file_dep': list(ctxt.get_objects()),
+            'targets': [f"{ctxt.output}"],
+            'clean': True,
+            'verbosity': 2
+        }
 
 if __name__ == '__main__':
     import doit
